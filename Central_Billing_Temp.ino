f4df9e87bc58cdd4d8d596620fdca3aa13c29c2d
rakib1295 <rakib1295@gmail.com>
2020-01-03 00:34:00 +0600

    FS

--- Central_Billing_Temp.ino
+++ Central_Billing_Temp.ino
@@ -1,17 +1,28 @@
+#include <FS.h>    
 #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 
 //needed for library
 #include <DNSServer.h>
-include <ESPAsyncWebServer.h>
-//#include <ESP8266WebServer.h>
-#include <WiFiManager.h>  
-include <ESPAsyncTCP.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h> 
+#include <ArduinoJson.h>
+#include <ESP8266mDNS.h>
+
 //https://github.com/tzapu/WiFiManager
 
 //for LED status
 #include <Ticker.h>
 Ticker ticker;
 
+struct ConfigData
+{
+  String Title;
+  String Name;
+  double CriticalTemp;
+  double HiCriticalHum;
+  double LowCriticalHum;
+  int PhnNumbers[10];
+} _configdata;
 
 char webpage[] PROGMEM = R"rawliteral(
 <html>
@@ -22,6 +33,14 @@ char webpage[] PROGMEM = R"rawliteral(
 <h1 align="middle">Config SMS Settings</h1>
   <form align="middle">
     <fieldset>
+      <div>
+        <label for="Title">Title of Portal: </label>      
+        <input id="Title" type="text" placeholder="Title">
+      </div><br>
+      <div>
+        <label for="Name">Name of Portal: </label>      
+        <input id="Name" type="text" placeholder="Name">
+      </div><br>
       <div>
         <label for="CriticalTemp">Critical Temperature: </label>      
         <input id="CriticalTemp" type="number" placeholder="Critical Temperature">°C</input>
@@ -77,6 +96,8 @@ char webpage[] PROGMEM = R"rawliteral(
   function myFunction()
   {
     console.log("button was clicked!");
+    var Title = document.getElementById("Title").value;
+    var Name = document.getElementById("Name").value;
     var CriticalTemp = document.getElementById("CriticalTemp").value;
     var HiCriticalHum = document.getElementById("HiCriticalHum").value;
     var LowCriticalHum = document.getElementById("LowCriticalHum").value;
@@ -94,7 +115,7 @@ char webpage[] PROGMEM = R"rawliteral(
     }
       
     
-    var data = {CriticalTemp:CriticalTemp, HiCriticalHum:HiCriticalHum, LowCriticalHum:LowCriticalHum, PhnNumbers:phndata};
+    var data = {Title:Title, Name:Name, CriticalTemp:CriticalTemp, HiCriticalHum:HiCriticalHum, LowCriticalHum:LowCriticalHum, PhnNumbers:phndata};
     console.log(data);
     var xhr = new XMLHttpRequest();
     var url = "/settings";
@@ -175,9 +196,109 @@ void setup() {
   //keep LED on if low
   digitalWrite(BUILTIN_LED, LOW);
   Serial.println("now: "+WiFi.SSID());
+
+  //////////////////////////////////////////////////////////end of WIFI part
+
+  server.on("/config",[](){server.send_P(200,"text/html", webpage);});
+  //server.on("/settings", HTTP_POST, WriteToFS);
+  server.on("/", getData);
+  server.onNotFound(handle_NotFound);
+
+
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+//void WriteToFS()
+//{
+//  String data = server.arg("plain");
+//  DynamicJsonBuffer jBuffer;
+//  JsonObject& jObject = jBuffer.parseObject(data);
+//
+//  Serial.println("Write Data to config.json file ... \n");
+//  File configFile = SPIFFS.open("/config.json", "w");
+//  if(!configFile){
+//    Serial.println("- failed to open file for writing");
+//    return;
+//  }
+//  jObject.printTo(configFile);  
+//  configFile.close();
+//  
+//  server.send(200, "application/json", "{\"status\" : \"ok\"}");
+//  delay(500);
+//  
+//}
 
+void ReadFromFS()
+{
+  if(SPIFFS.exists("/config.json"))
+  {
+
+    File configFile = SPIFFS.open("/config.json", "r");
+    if(configFile)
+    {
+      Serial.print("Reading Data from Config File.....\n");
+      size_t size = configFile.size();
+      std::unique_ptr<char[]> buf(new char[size]);
+      configFile.readBytes(buf.get(), size);
+      configFile.close();
+
+      DynamicJsonBuffer jsonBuffer;
+      JsonObject& jObject = jsonBuffer.parseObject(buf.get());
+      if(jObject.success())
+      {
+        _configdata.Title = jObject["Title"];
+        _configdata.Name = jObject["Name"];
+        
+        Serial.print("\nConfig File data: ");
+        Serial.println(_configdata.Title);
+        Serial.println(_configdata.Name);
+      }
+    }
+}
+
+void getData() {
+
+  Temperature = dht.readTemperature(); // Gets the values of the temperature
+  Humidity = dht.readHumidity(); // Gets the values of the humidity
+
+  Serial.println("Temperature data.. ");
+  Serial.println(Temperature);
+  server.send(200, "text/html", showDataHTML(Temperature, Humidity));
+}
+
+String showDataHTML(float Temperaturestat, float Humiditystat) 
+{
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr += "<title>Central Billing Sher-e-Bangla</title>\n";
+  ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
+  ptr += "p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
+  ptr += "</style>\n";
+  ptr += "</head>\n";
+  ptr += "<body>\n";
+  ptr += "<div id=\"webpage\">\n";
+  ptr += "<h1>Central Billing Sher-e-Bangla</h1>\n";
+
+  ptr += "<p>Temperature: ";
+  ptr += (int)Temperaturestat;
+  ptr += "°C</p>"; 
+  ptr += "<p>Humidity: ";
+  ptr += (int)Humiditystat;
+  ptr += "%</p>";
+
+  ptr += "</div>\n";
+  ptr += "</body>\n";
+  ptr += "</html>\n";
+  return ptr;
+}
+
+void handle_NotFound() 
+{
+  server.send(404, "text/plain", "Not found");
+}
+
+void loop() 
+{
+  // put your main code here, to run repeatedly:
+  server.handleClient();
 }
